3c10b1d3f0baca03f156656e63312e3b626f3a6f
hussameldin-abubakr <57759831+hussameldin-abubakr@users.noreply.github.com>
2024-06-04 15:13:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let currentImageIndex = 0;
 const images = [
-    "images/1.jpg", 
-    "images/2.jpg",
-    "images/3.jpg"
+    "1.jpg", 
+    "2.jpg",
+    "3.jpg"
     // Add more images as needed
 ];
 
